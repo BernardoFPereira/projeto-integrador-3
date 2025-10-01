996177eec5d9c2aaf8e2dbc3d57b04db2d97b59e
Vini Baeta <vbaeta2015@gmail.com>
2025-10-01 17:26:41 -0300

    Cutscenes
    added assets for cutscene, began animating cs01

--- autoloads/progress_manager.gd
+++ autoloads/progress_manager.gd
@@ -7,5 +7,5 @@ var past_first_contact := false
 
 var journal: Journal
 
-func _ready() -> void:
-	journal = get_tree().get_first_node_in_group("UI").find_child("Journal")
+#func _ready() -> void:
+	#journal = get_tree().get_first_node_in_group("UI").find_child("Journal")
