cae869d0b54600b59008190bec612bf5eae7cd02
Vini Baeta <vbaeta2015@gmail.com>
2025-11-06 14:45:40 -0300

    edited cutscene 01
    Ao acabar a cutscene, comeca o Level Monastery Inside
    
    Adicionado texto rescunho ao comeco da cutscene 01

---
+++ cutscenes/level_manager.gd
@@ -0,0 +1,10 @@
+extends Node
+
+@onready var animation_player = $SceneAnimationPlayer
+
+func _ready():
+	animation_player.animation_finished.connect(_on_animation_finished)
+
+func _on_animation_finished(anim_name):
+	if anim_name == "MainScene":
+		get_tree().change_scene_to_file("res://scenes/levels/MonasteryInside.tscn")
